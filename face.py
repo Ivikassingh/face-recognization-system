a47e850014315bbc4886bf339912e4aea9bfef4c
mvikaskumar <vikas.r.kumar.9@gmail.com>
2018-09-23 12:03:24 +0530

    Add files via upload

---
+++ face.py
@@ -0,0 +1,18 @@
+#numpy model script to run opencv
+
+import cv2
+import numpy as np
+faceDetect=cv2.CascadeClassifier("name of file");
+cam=cv2.VideoCapture(0);
+
+while(True):
+    ret.img=cam.read();
+    gray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    faces=faceDetect.detectMultiScale(gray,1,3,5);
+    for(x,y,w,h in faces):
+        cv2.rectangle(img,(x,y),(x+w,y+h),(0,0,0),2)
+    cv2.imshow("Face",img);
+    if(cv2.waitKey(1)==ord('q')):
+        break;
+cam.release()
+cv2.destroyAllWindow()
